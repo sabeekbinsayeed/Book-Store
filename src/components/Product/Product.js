a79eb15dc033d99467d0a4c25433e93ac2c8111b
sabeekbinsayeed <sabikbinsayeed@gmail.com>
2022-03-26 01:06:29 +0600

    second

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Product = (props) => {
+    //console.log(props)
+
     const { name, img, price, id } = props.product
+
+
+
     return (
         <div>
             <img src={img}></img>
             <h1>{name}</h1>
+            <h3>{price}</h3>
+            <button onClick={() => { props.addToCart(props.product) }}><p>Add to Cart</p></button>
 
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,15 +4,25 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [carts, setCarts] = useState([])
     useEffect(() => {
         fetch('products.json').then(res => res.json()).then(data => setProducts(data))
     }, [])
+
+    const addToCart = myproduct => {
+        console.log(myproduct)
+        // cons//ole.log(id)
+
+        const newCart = [...carts, myproduct]
+        setCarts(newCart)
+        console.log(newCart)
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
 
                 {
-                    products.map(product => <Product product={product} key={product.id}></Product>)
+                    products.map(product => <Product product={product} key={product.id} addToCart={addToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
