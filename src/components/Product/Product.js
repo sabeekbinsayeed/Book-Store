d5624ddb983d1b53e6ac62d156b5a5d1f988e0ed
sabeekbinsayeed <sabikbinsayeed@gmail.com>
2022-03-26 22:19:49 +0600

    last

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
 
 const Product = (props) => {
     //console.log(props)
@@ -12,7 +15,10 @@ const Product = (props) => {
             <img src={img}></img>
             <h1>{name}</h1>
             <h3>{price}</h3>
-            <button onClick={() => { props.addToCart(props.product) }}><p>Add to Cart</p></button>
+            <button className='cart-button' onClick={() => { props.addToCart(props.product) }}><p>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+
+            </button>
 
         </div>
     );
